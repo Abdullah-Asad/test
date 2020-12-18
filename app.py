506e8d4f5a835e763d38e10305a13f341a3fb898
Abdullah-Asad <75277035+Abdullah-Asad@users.noreply.github.com>
2020-12-18 18:50:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,48 @@
+import cv2
+import tensorflow as tf
 import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
+from flask import Flask, request
+
+CATEGORIES = ["Healthy", "Effected"]
+
+
+def prepare(file):
+    IMG_SIZE = 50
+    new_array = cv2.resize(file, (IMG_SIZE, IMG_SIZE))
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+
+
+def load_model():
+    global model
+
+    model = tf.keras.models.load_model("CNN.h5")
+
 
 @app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+def home_endpoint():
+    return 'Hello World!'
 
 
-if __name__ == "__main__":
+@app.route('/predict', methods=['POST'])
+def get_prediction():
+    # Works only for a single sample
+    if request.method == 'POST':
+        print("1")
+        # user_image = request.files["file"]
+        # image = cv2.imdecode(np.frombuffer(user_image.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
+        image = cv2.imread('image.png', 0)
+        preparedImage = prepare(image)
+
+        prediction = model.predict([preparedImage])
+        print("6")
+        prediction = list(prediction[0])
+
+        return CATEGORIES[prediction.index(max(prediction))]
+
+
+if __name__ == '__main__':
+    load_model()  
     app.run(debug=True)
